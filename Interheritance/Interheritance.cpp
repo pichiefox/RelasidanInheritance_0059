15a46e1973068576fbc7296401aca8ca9b4fe9fd
pichiefox <citrazad@gmail.com>
2026-05-12 18:17:54 +0700

    Interheritance Done

--- Interheritance/Interheritance.cpp
+++ Interheritance/Interheritance.cpp
@@ -30,8 +30,8 @@ public:
         cout << "pelajar dihapus\n" << endl;
     }
     string perkenalan() {
-        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n
-\n"}
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n \n";
+    }
 };
 
 int main() {
