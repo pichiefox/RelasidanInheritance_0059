781eda309708e30e804887b644bf28af4422cef2
pichiefox <citrazad@gmail.com>
2026-05-12 17:56:08 +0700

    Program Utama

--- Interheritance/Interheritance.cpp
+++ Interheritance/Interheritance.cpp
@@ -34,3 +34,10 @@ public:
 \n"}
 };
 
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1. jumlah(10, 90) << endl;
+
+    return 0;
+}
