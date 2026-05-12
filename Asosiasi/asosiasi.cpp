85d8913313ab3f3255459757bca4df4f47957dad
pichiefox <citrazad@gmail.com>
2026-05-12 17:28:56 +0700

    Class Pasien dan Propertinya

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -3,3 +3,16 @@
 using namespace std;
 
 class dokter; //pembuatan class dokter terlebih dahulu agar tidak error
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
