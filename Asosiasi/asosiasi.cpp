aefcd1d19155b584b1a92d61f67ad6dd740fb50d
pichiefox <citrazad@gmail.com>
2026-05-12 17:39:26 +0700

    Function

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -33,3 +33,28 @@ public:
     void cetakPasien();
 };
 
+void pasien: :tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien: : cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for 
+                                    // untuk secara otomatis menentukan tipe data 
+                                    // dari elemen yang diiteras
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter :: tambahPasien(pasien* pPasien) {
+daftar_pasien.push_back(pPasien);
+pPasien->tambahDokter(this);
+}
+void dokter: : cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+     cout << endl;
+}
+
